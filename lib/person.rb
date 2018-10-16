91faf2461b25810295850ed05cb697ba2935acc0
Angelo zarate <mrlzarate8@gmail.com>
2018-10-16 09:18:24 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def name
+    @name
+  end
+  def name=(this_dog)
+    @name=this_dude
+  end
+  def breed
+    @breed
+  end
+  def breed=(this_breed)
+    @breed=this_breed
+  end
+end
